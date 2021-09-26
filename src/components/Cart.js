ddc646480fb698adc6266254e88d4839858d6dc1
ArpitaMohajan <arpitamohajan2000moon@gmail.com>
2021-09-26 22:17:34 +0600

    one

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -6,10 +6,21 @@ const Cart = (props) => {
     const totals = cart.reduce(cartReduce, 0)
     // console.log(props)
     return (
+
         <div>
-            <h1>Order Summary</h1>
+            <div className="col-sm-8">
+                <div className="card">
+                    <div className="card-body">
+                        <h3>Order Summary</h3>
+                        <p>Items Order:{props.cart.length}</p>
+                        <h3>Total:{totals.toFixed(2)}</h3>
+                        <a className="btn btn-success">Buy Now</a>
+                    </div>
+                </div>
+            </div>
+            {/* <h1>Order Summary</h1>
             <p>Items Order:{props.cart.length}</p>
-            <h3>Total:{totals.toFixed(2)}</h3>
+            <h3>Total:{totals.toFixed(2)}</h3> */}
         </div>
     );
 };
